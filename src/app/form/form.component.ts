80e1f515d8ae2173263e94257a5394c6c73fc35c
RonSalvet <ronaldsalvatierraflores@gmail.com>
2025-04-01 22:24:02 -0400

    first commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Formulario enviado', this.form.value);
+    }
+  }
+}
+
